ee179cc97f44241a37af1d585dda3a4fbc28c1e1
Artemii Stobalov <artemiystebalov@outlook.com>
2018-12-23 21:44:41 +0200

    new changes

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ function createChat()
     var ChatNumber=1;
     var chatMassage = [];
     var iframeMembers = [];
-    var iframe, buttonSend, input, iframeName, name;
+    var iframe, buttonSend, input, iframeName, name, close;
 
     buttonAdd.onclick  = ()=>{
         function createIframe()
@@ -29,30 +29,71 @@ function createChat()
         function iframeElements()
             {
                 var iframeDocument = window.frames[iframeName].document;
+                var headerChat = iframeDocument.createElement('div');
+                var gettingName = function(){ 
+                    var nc = prompt('Name chat', '');
+                    if (nc == ''){
+                        return gettingName();
+                        }
+                    else{
+                        return nc;
+                        }
+                }
+                chatName = gettingName();
+                headerChat.innerText = chatName;
+                headerChat.style.fontWeight	 = 'bold'
+                headerChat.style.textAlign = 'center';
+                headerChat.style.color = 'blue';
+                headerChat.style.position = 'fixed';
+                headerChat.style.width = '290px';
+                headerChat.style.top = '0px';
+                headerChat.style.background = '#fff';
+                close = iframeDocument.createElement('span');
+                close.id  = 'chat' + ChatNumber;
+                close.innerHTML = 'X';
+                close.style.cursor = 'pointer';
+                close.style.right = '6%';
+                close.style.position = 'absolute'
                 buttonSend = iframeDocument.createElement('button');
                 buttonSend.style.bottom = '0%';
                 buttonSend.style.position = 'fixed'
+                buttonSend.style.width = '19%'
+                buttonSend.style.left = '2%';
                 input = iframeDocument.createElement('input');
+                input.style.width = '78%'
                 input.style.bottom = '0%';
-                input.style.right = '10%';
+                input.style.right = '2%';
                 input.style.position = 'fixed';
                 iframes = window.frames[iframeName].document;
+                
+                iframes.body.appendChild(headerChat);
                 iframes.body.appendChild(buttonSend);
                 iframes.body.appendChild(input);
+                headerChat.appendChild(close);
                 name = iframeName;
-                buttonSend.innerText = name;
-                assignSendMessageHandler(buttonSend, input, name);
+                buttonSend.innerText = 'Send';
+                assignSendMessageHandler(buttonSend, input, name, ChatNumber, chatName);
                 ChatNumber++;
             }
         createIframe()
     }
 
-    function assignSendMessageHandler(btn, input , nm) {
+    function assignSendMessageHandler(btn, input , nm, ChatNumber, chatName) {
+        
+        close.onclick = ()=>{
+            var iframeRemove = document.getElementById(nm);
+            div.removeChild(iframeRemove);
+            var idx = iframeMembers.indexOf(ChatNumber);
+            iframeMembers.splice(idx, 1);
+
+        } 
+
+
         btn.addEventListener('click', function() {
             sendMessage(input.value)
             input.value = '';
             name = nm;
-            
+            cN = chatName;
         });
         
     var chatMove = document.getElementById(nm);
@@ -97,27 +138,29 @@ function createChat()
         function notifyAllFrames(msg) {
             chatMassage.push(msg);
             iframeMembers.forEach(function(items){
-            chat = document.createElement('p');
-            chat.style.background = 'lightblue';
-            chat.style.borderRadius = '10%';
-            iframeName ='iframe'+items;
-            iframe.name = iframeName;
-            iframes = window.frames[iframeName].document;
-            var iframeBody = iframes.body;
-            iframeBody.appendChild(chat);
-            iframeBody.style.marginBottom = '60px';
-            iframeBody.scrollTop = 9999;
-            
-            for ( var i = 0 ; i < chatMassage.length; i++ ){
-                if(iframeName== name){
-                    chat.innerHTML = '';
-                    chat.style.background = 'rgb(102, 255, 0)';
-                    chat.style.textAlign = 'right';
-                    chat.innerText = chatMassage[i] ;
-                }
-                else{
-                chat.innerHTML = '';
-                chat.innerText = name + ':' + ' '+ chatMassage[i];
+                
+                chat = document.createElement('p');
+                chat.style.background = 'lightblue';
+                chat.style.borderRadius = '10%';
+                iframeName ='iframe'+items;
+                iframe.name = iframeName;
+                iframes = window.frames[iframeName].document;
+                var iframeBody = iframes.body;
+                iframeBody.appendChild(chat);
+                iframeBody.style.marginBottom = '60px';
+                iframeBody.style.marginTop = '25px';
+                iframeBody.scrollTop = 9999;
+                
+                for ( var i = 0 ; i < chatMassage.length; i++ ){
+                    if(iframeName== name){
+                        chat.innerHTML = '';
+                        chat.style.background = 'rgb(102, 255, 0)';
+                        chat.style.textAlign = 'right';
+                        chat.innerText = chatMassage[i] ;
+                    }
+                    else{
+                        chat.innerHTML = '';
+                        chat.innerText = cN + ':' + ' '+ chatMassage[i];
             }}})};
 
         window.addEventListener('message', function(event) {
@@ -125,6 +168,7 @@ function createChat()
             {
             notifyAllFrames(event.data)
             }
+            
             else{
                 return
             }
@@ -133,3 +177,4 @@ function createChat()
     assignReceiveMessageHandler();
 }
 createChat()
+//https://github.com/elArt/Challange
